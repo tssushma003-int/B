63bd3020f563625ea31284da896c17a8e20e477c
tssushma003-int <tssushma003@gmail.com>
2026-09-11 13:04:20 +0530

    ab

---
+++ main.java
@@ -0,0 +1,25 @@
+ class main{
+    public static void main (String[] args)
+    {
+        int a=10,b=20;
+        System.out.println(a+b);
+
+        int x=100,y=50;
+        System.out.println(y-x);
+
+        double p=20,q=30;
+        System.out.println(p*q);
+
+        int a1=30,b1=10;
+        System.out.println(a1/b1);
+
+        int l=17,m=3;
+        System.out.println(l%m);
+       
+        int ic=10,jc=20;
+        System.out.println(ic++ + jc++ + ++ic + ++jc + ++ic);
+
+
+
+    }
+ }
